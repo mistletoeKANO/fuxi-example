dce975ce8be685cc8da30757a4ec86d1608d2a43
Mono <1649881580@qq.com>
2022-08-15 14:08:04 +0800

    新增 版本更新日志, 新增 自定义包名拓展接口

---
+++ Assets/Example/Editor/CustomPlayerName.cs
@@ -0,0 +1,10 @@
+﻿using FuXi.Editor;
+
+[PlayerNamePriority(-1)]
+public class CustomPlayerName : IPlayerNameDefine
+{
+    public string GetPlayerName(string version)
+    {
+        return "Custom";
+    }
+}
